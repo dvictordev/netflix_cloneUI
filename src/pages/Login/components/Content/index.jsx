2afefc5886fcb7fc7f6d51955b722a44e5d1219f
Victor Duarte <victor.d.dev@gmail.com>
2022-05-14 11:55:24 -0300

    added login page form

--- src/pages/Login/components/Content/index.jsx
+++ src/pages/Login/components/Content/index.jsx
@@ -25,8 +25,6 @@ const loginPageContent={
   },
 }
 
-
-
 export function Content(){
   return(
     <>
--- src/pages/Login/components/Header/index.jsx
+++ src/pages/Login/components/Header/index.jsx
@@ -1,5 +1,8 @@
 import logoImg from '../../../../assets/netflix_logo.png'
 import backgroundImg from '../../../../assets/background_image.jpg'
+import {CaretRight} from 'phosphor-react'
+import { useNavigate } from 'react-router-dom'
+import { Home } from '../../../Home'
 
 const backgroundStyle ={
   backgroundImage:`url(${backgroundImg})`,
@@ -10,17 +13,37 @@ const backgroundStyle ={
 
 }
 
+
+function handleGetIn(){
+  let navigate = useNavigate();
+  navigate('../')
+}
+
 export function Header(){
   return(
     <div style={backgroundStyle} className='border-b-[6px] border-zinc-500'>
       <header className='flex w-full px-14 pt-7 items-center justify-between'>
         <img src={logoImg} alt="logo netflix" className='w-[166px] h-12'/>
-        <button className='w-16 h-10 bg-red-700 text-zinc-100 rounded-lg border-0 hover:border-2 hover:border-zinc-100'>Entrar</button>
+        <button
+          onClick={handleGetIn}
+          className='w-20 h-10 bg-red-700 text-zinc-100  text-center rounded-sm border-0 hover:border-2 hover:border-zinc-100'
+         >Entrar</button>
       </header>
-      <main>
-        <h1>Filmes, séries e muito mais. Sem limites.</h1>
-        <span>Assista onde quiser. Cancele quando quiser.</span>
-        <p>Pronto para assistir? Informe seu email para criar ou reiniciar sua assinatura.</p>
+      <main className='w-auto h-[755px] flex flex-col items-center justify-center text-zinc-100 text-center gap-5 lg:px-96 md:py-20 '>
+        <h1 className='font-bold sm:text-lg lg:text-7xl'>Filmes, séries e muito mais. Sem limites.</h1>
+        <span className='text-2xl font-bold'>Assista onde quiser. Cancele quando quiser.</span>
+        <p className='text-xl font-bold'>Pronto para assistir? Informe seu email para criar ou reiniciar sua assinatura.</p>
+
+        <div className='flex items-center justify-center'>
+          <input
+            placeholder='Email...'
+            type="text" 
+            className='p-4 h-16 w-[500px] text-zinc-700 outline-none'/>
+          <button className='flex items-center justify-center bg-red-700 p-4 w-44 h-16 font-bold text-xl gap-2'>
+            Vamos la
+            <CaretRight weight='bold' className='mt-[1px]'/>
+          </button>
+        </div> 
         
       </main>
 
