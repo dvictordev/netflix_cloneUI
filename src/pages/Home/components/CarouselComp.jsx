e628941658ddf2fb3a8d6cdf2b11ea185e5f804e
Victor Duarte <victor.d.dev@gmail.com>
2022-05-16 10:58:57 -0300

    modified carousel

--- src/pages/Home/components/CarouselComp.jsx
+++ src/pages/Home/components/CarouselComp.jsx
@@ -1,42 +1,57 @@
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
+// import Slider  from 'react-slick'
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+
+
+// import required modules
+import { Navigation } from "swiper";
+
 import React from "react";
-import Slider  from 'react-slick'
 import { Movie } from "./Movie"
 import { useApi } from "../../../hooks/useApi"
 
 export function CarouselComp(){
+  // request api data
   const data = useApi()
-  var settings = {
-    dots: false,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 1,
-    slidesToScroll: 2,
-    centerMode:true,
-    centerPadding: '450px',
-    rtl:true,
-  };
 
-  const baseImgUrl = 'https://image.tmdb.org/t/p/original/'
+
+  // settings carousel
+  // var settings = {
+  //   dots: false,
+  //   infinite: true,
+  //   speed: 300,
+  //   slidesToShow: 1,
+  //   slidesToScroll: 2,
+  //   centerMode:true,
+  //   centerPadding: '480px',
+  //   rtl:true,
+  // };
+  // img base url
+  const baseImgUrl = 'https://image.tmdb.org/t/p/w300'
         
-
 
   return (
     <div className="w-full flex items-center justify-center mt-10">
-
-    <Slider {...settings} className='w-[1200px]'>
+    <Swiper
+      spaceBetween={10}
+      slidesPerView={5}
+      navigation={true} 
+      modules={[Navigation]}
+    >
      {data.map(movie=> {
-       console.log(movie)
           return( 
-            <div>
-              <Movie key={movie.id} title={movie.title} poster_path={`${baseImgUrl}${movie.poster_path}`}/>   
-            </div> 
+            <SwiperSlide>
+              <Movie key={movie.id} title={movie.title} poster_path={`${baseImgUrl}${movie.backdrop_path}`}/>   
+            </SwiperSlide>   
           )
         })}
-
-      
-    </Slider>
+    </Swiper>
     </div>
   )
 }
--- src/pages/Home/components/Movie.jsx
+++ src/pages/Home/components/Movie.jsx
@@ -1,13 +1,15 @@
 export function Movie({title, overview, vote_average, poster_path }){
   const backCardImg={
     backgroundImage:`url(${poster_path})`,
-    backgroundSize:'cover',
+    backgroundSize:'206px 144px',
     backgroundRepeat:'no-repeat'
 
   }
+  console.log(poster_path)
 
   return(
     <div className="w-64 h-36" style={backCardImg}>
+      
       <h1 className="text-zinc-100">{title}</h1>
     </div>
   )
